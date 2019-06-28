1d705194cd858f4724c60e0063b1635c5cbb5ae4
mmheydari97 <heidarymm@yahoo.com>
2019-06-28 15:09:07 +0430

    build db

--- pymongochat/main.py
+++ pymongochat/main.py
@@ -15,12 +15,33 @@ with client:
     if ans.lower() in ["yes", "y"]:
         print("enter a username to login:")
         username = input()
-        print("now enter pass word:")
+        print("now enter password:")
         password = input()
 
-
+        try:
+            cur_user = db.users.find({
+                '$and': [{"username": username}, {"password": password}]
+            }).next()
+        except StopIteration:
+            print("incorrect username or password.")
 
     elif ans.lower() in ["no", "n"]:
         print("enter a username to signup:")
         username = input()
+        print("now enter password:")
+        password = input()
 
+        a = db.users.insert({
+            "username": username,
+            "password": password,
+            "contacts": [],
+            "channels": []
+        })
+        cur_user = db.users.find({
+            '$and': [{"username": username}, {"password": password}]
+        }).next()
+
+    print("enter next command or quit: ")
+    command = input().lower()
+    while command != "qiut":
+        
